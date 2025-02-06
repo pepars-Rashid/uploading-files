edd697d5a9bf5f5d4440496a91dba6c869d60864
pepars-Rashid <peparsrashid@gmail.com>
2025-02-06 13:18:51 -0800

    other imports

--- src/components/product-form.jsx
+++ src/components/product-form.jsx
@@ -3,10 +3,10 @@
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
-import { Button } from '@/components/ui/button';
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
-import { Input } from '@/components/ui/input';
-import { Textarea } from '@/components/ui/textarea';
+import { Button } from './ui/button';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from './ui/form';
+import { Input } from './ui/input';
+import { Textarea } from './ui/textarea';
 import { SingleImageDropzone } from './singleImageDropzone';
 import { useEdgeStore } from '@/lib/edgestore';
 import { useState } from 'react';
