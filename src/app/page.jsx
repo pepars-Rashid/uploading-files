656b90f0ec39d855475e0b51d5e0d0d2124dfe82
pepars-Rashid <peparsrashid@gmail.com>
2025-04-11 16:37:53 +0300

    long video

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,7 +6,7 @@ export default function Page() {
   return (
     <div>
       <video controls>
-        <source src="https://files.edgestore.dev/0jvce83pji4k8o75/publicFiles/_public/8f8b8d35-3448-4419-82fd-e230f36c2fe9.mp4" type="video/mp4"/>
+        <source src="https://files.edgestore.dev/0jvce83pji4k8o75/publicFiles/_public/ae2340f2-8154-4d65-99ce-f75702b4d15f.mp4" type="video/mp4"/>
     </video>
     </div>
   )
