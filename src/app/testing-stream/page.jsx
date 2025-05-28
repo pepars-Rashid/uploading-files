8976b6956af67e087ccb204db1ec715a8ce450d3
pepars-Rashid <peparsrashid@gmail.com>
2025-05-28 12:51:55 +0300

    dierect url

--- src/app/testing-stream/page.jsx
+++ src/app/testing-stream/page.jsx
@@ -4,7 +4,7 @@ import SimpleHLSPlayer from "@/components/hls-player";
 export default function Page() {
   return (
     <>
-        <SimpleHLSPlayer url="https://files.edgestore.dev/0jvce83pji4k8o75/publicFiles/_public/41438c98-3c1d-4d08-8cf0-1a8f4dcba77b.m3u8" />
+        <SimpleHLSPlayer url="https://stream.mux.com/iDwVSFAUhLQle02iPxozwaVqeeQiJDruVp85NB8Q4E68.m3u8?token=eyJhbGciOiJSUzI1NiJ9.eyJraWQiOiJJbEpXWGlLcTAxR2ZBdnpoMWpRaDkyV3BDQzFQY0ZMcnF4eDAxeUUwMk5pOFpNIiwic3ViIjoiaUR3VlNGQVVoTFFsZTAyaVB4b3p3YVZxZWVRaUpEcnVWcDg1TkI4UTRFNjgiLCJhdWQiOiJ2IiwiZXhwIjoxNzQ5MDI5OTI1fQ.DD2MvoR9dOttWvP_QeLN5cY48xXbj0l18WYmzYSwDln6igXm-75dg7VN0d7M0jkae1xcksGpB9ax4kijES4yJlvDQvG2UsW0XMeGoc9Lm4dXp_z1lTNN1trTWTgRjQlTvfiGsMEaWGaH-2boh29u17VIl2YRVU-7ZsiOB_X-lqLUxkwy_ceEtjF-Xu7f5lkJQv6uY1l_XJPrLYx4k9Tk_p43lNeCX9s3uOGSjYpUARrXeMfjruBN4nsRUzc_XJkWbw8fgs4UZTBIgwQYgzNKhopCpHsHXCxs2ziVkn-4rG__lT2kNSm5uxlIB-ue0vfF8kWUTO23erR3ufV5qEXCqA" />
     </>
   );
 }
